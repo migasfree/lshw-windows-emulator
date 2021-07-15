8ee858bff872bb4d986493857e288cf74f88f333
Jose Antonio Chavarría <jachavar@gmail.com>
2021-07-15 11:19:20 +0200

    Removed extra quotes

--- bin/create-package.bat
+++ bin/create-package.bat
@@ -5,7 +5,7 @@ python setup.py bdist_wininst
 python setup.py bdist
 python setup.py bdist --format=msi
 
-echo "Python requirements:"
-echo "  * Python >= 3.6"
-echo "      - psutil"
-echo "      - wmi"
+echo Python requirements:
+echo   * Python >= 3.6
+echo       - psutil
+echo       - wmi
