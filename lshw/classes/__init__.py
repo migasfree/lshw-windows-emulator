4fe555c947f9067f441e7cc2bb98c733fc2933d5
jact <jachavar@gmail.com>
2026-01-19 14:16:14 +0100

    refactor: ruff guidelines

--- lshw/classes/__init__.py
+++ lshw/classes/__init__.py
@@ -1,6 +1,4 @@
-# -*- coding: UTF-8 -*-
-
-# Copyright (c) 2021-2024 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2021-2026 Jose Antonio Chavarría <jachavar@gmail.com>
 # Copyright (c) 2011-2021 Alfonso Gómez Sánchez <agomez@zaragoza.es>
 #
 # This program is free software: you can redistribute it and/or modify
@@ -16,26 +14,43 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-__author__ = [
-    'Jose Antonio Chavarría <jachavar@gmail.com>',
-    'Alfonso Gómez Sánchez <agomez@zaragoza.es>'
-]
+__author__ = ['Jose Antonio Chavarría <jachavar@gmail.com>', 'Alfonso Gómez Sánchez <agomez@zaragoza.es>']
 __license__ = 'GPLv3'
 
-from .hardware_class import HardwareClass
 from .base_board import BaseBoard
+from .cd_rom import CdRom
+from .computer_system import ComputerSystem
 from .firmware import Firmware
-from .processor import Processor
-from .physical_memory import PhysicalMemory
+from .graphic_card import GraphicCard
+from .hardware_class import HardwareClass
+from .ide import Ide
+from .logical_disk import LogicalDisk
+from .network_card import NetworkCard
+from .partition_disk import PartitionDisk
 from .pci import Pci
 from .physical_disk import PhysicalDisk
-from .partition_disk import PartitionDisk
-from .ide import Ide
-from .cd_rom import CdRom
-from .logical_disk import LogicalDisk
-from .usb_device import UsbDevice
-from .usb import Usb
+from .physical_memory import PhysicalMemory
+from .processor import Processor
 from .sound_device import SoundDevice
-from .graphic_card import GraphicCard
-from .network_card import NetworkCard
-from .computer_system import ComputerSystem
+from .usb import Usb
+from .usb_device import UsbDevice
+
+__all__ = [
+    'BaseBoard',
+    'CdRom',
+    'ComputerSystem',
+    'Firmware',
+    'GraphicCard',
+    'HardwareClass',
+    'Ide',
+    'LogicalDisk',
+    'NetworkCard',
+    'PartitionDisk',
+    'Pci',
+    'PhysicalDisk',
+    'PhysicalMemory',
+    'Processor',
+    'SoundDevice',
+    'Usb',
+    'UsbDevice',
+]
