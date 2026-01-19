94cf33c9e8b63426a761833daf0c8c829dbcf274
jact <jachavar@gmail.com>
2026-01-19 14:19:23 +0100

    refactor: ruff guidelines

--- lshw/classes/firmware.py
+++ lshw/classes/firmware.py
@@ -1,6 +1,4 @@
-# -*- coding: UTF-8 -*-
-
-# Copyright (c) 2021 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2021-2026 Jose Antonio Chavarría <jachavar@gmail.com>
 # Copyright (c) 2011-2021 Alfonso Gómez Sánchez <agomez@zaragoza.es>
 #
 # This program is free software: you can redistribute it and/or modify
@@ -16,10 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-__author__ = [
-    'Jose Antonio Chavarría <jachavar@gmail.com>',
-    'Alfonso Gómez Sánchez <agomez@zaragoza.es>'
-]
+__author__ = ['Jose Antonio Chavarría <jachavar@gmail.com>', 'Alfonso Gómez Sánchez <agomez@zaragoza.es>']
 __license__ = 'GPLv3'
 
 from .hardware_class import HardwareClass
@@ -48,15 +43,10 @@ class Firmware(HardwareClass):
             'serial': self.__ERROR__,
             'units': '',
             'size': 0,
-            'capacity': 0
+            'capacity': 0,
         }
 
-        self.properties_to_get = [
-            'Manufacturer',
-            'BIOSVersion',
-            'ReleaseDate',
-            'SerialNumber'
-        ]
+        self.properties_to_get = ['Manufacturer', 'BIOSVersion', 'ReleaseDate', 'SerialNumber']
 
         self._update_properties_to_return()
 
@@ -64,15 +54,9 @@ class Firmware(HardwareClass):
         self.get_hardware()
 
         for hw_item in self.hardware_set_to_return:
-            self.formatted_data['vendor'] = hw_item.get(
-                'Manufacturer', self.__ERROR__
-            )
-            self.formatted_data['date'] = hw_item.get(
-                'ReleaseDate', self.__ERROR__
-            )
-            self.formatted_data['serial'] = hw_item.get(
-                'SerialNumber', self.__ERROR__
-            )
+            self.formatted_data['vendor'] = hw_item.get('Manufacturer', self.__ERROR__)
+            self.formatted_data['date'] = hw_item.get('ReleaseDate', self.__ERROR__)
+            self.formatted_data['serial'] = hw_item.get('SerialNumber', self.__ERROR__)
             if 'BIOSVersion' in hw_item:
                 self.formatted_data['version'] = hw_item['BIOSVersion'][0]
 
