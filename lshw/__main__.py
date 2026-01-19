e8d429402ada93e8fe29cf47e31c12ac4f94a35a
jact <jachavar@gmail.com>
2026-01-19 14:15:40 +0100

    feat: added logging feature

--- lshw/__main__.py
+++ lshw/__main__.py
@@ -1,6 +1,4 @@
-# -*- coding: UTF-8 -*-
-
-# Copyright (c) 2021-2024 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2021-2026 Jose Antonio Chavarría <jachavar@gmail.com>
 # Copyright (c) 2011-2021 Alfonso Gómez Sánchez <agomez@zaragoza.es>
 #
 # This program is free software: you can redistribute it and/or modify
@@ -16,61 +14,64 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-__author__ = [
-    'Jose Antonio Chavarría <jachavar@gmail.com>',
-    'Alfonso Gómez Sánchez <agomez@zaragoza.es>'
-]
+__author__ = ['Jose Antonio Chavarría <jachavar@gmail.com>', 'Alfonso Gómez Sánchez <agomez@zaragoza.es>']
 __license__ = 'GPLv3'
 
-import sys
 import argparse
 import json
+import logging
+import sys
+
+import wmi
 
 from .classes import HardwareClass
+
+logger = logging.getLogger(__name__)
 
 PROGRAM = 'lshw'
 ALL_OK = 0
 
 AVAILABLE_CLASSES = {
-    "system": ['ComputerSystem', 1],
-    "baseboard": ['BaseBoard', 2],
-    "bios": ['Firmware', 2],
-    "processor": ['Processor', 3],
-    "memory": ['PhysicalMemory', 4],
-    "pci": ['Pci', 5],
-    "ide": ['Ide', 6],
-    "disk": ['PhysicalDisk', 7],
-    "partition": ['PartitionDisk', 8],
-    "volume": ['LogicalDisk', 9],
-    "usb": ['Usb', 10],
-    "usbdevices": ['UsbDevice', 11],
-    "cdrom": ['CdRom', 12],
-    "video": ['GraphicCard', 13],
-    "network": ['NetworkCard', 14],
-    "sound": ['SoundDevice', 15]
+    'system': ['ComputerSystem', 1],
+    'baseboard': ['BaseBoard', 2],
+    'bios': ['Firmware', 2],
+    'processor': ['Processor', 3],
+    'memory': ['PhysicalMemory', 4],
+    'pci': ['Pci', 5],
+    'ide': ['Ide', 6],
+    'disk': ['PhysicalDisk', 7],
+    'partition': ['PartitionDisk', 8],
+    'volume': ['LogicalDisk', 9],
+    'usb': ['Usb', 10],
+    'usbdevices': ['UsbDevice', 11],
+    'cdrom': ['CdRom', 12],
+    'video': ['GraphicCard', 13],
+    'network': ['NetworkCard', 14],
+    'sound': ['SoundDevice', 15],
 }
 
 
-def _exit_manager(exit_code, exit_element=''):
+def _exit_manager(exit_code, exit_element='', error_detail=None):
     """
     Exit management
     Print exit text
 
     exit_code: Number exit code
-    exit_element: Element afected by exit code
+    exit_element: Element affected by exit code
+    error_detail: Optional error message for debugging
     """
-
-    if exit_code >= 1 and exit_code <= 16:
-        _exit_message = 'there was an error getting "[element]" information'
+    if 1 <= exit_code <= 16:
+        _exit_message = f'there was an error getting "{exit_element}" information'
         if exit_code == 16:
             _exit_message = 'There was a critical error getting hardware information'
 
-        _exit_text = '[err #{}] {}'.format(
-            exit_code,
-            _exit_message.replace('[element]', exit_element)
-        )
+        _exit_text = f'[err #{exit_code}] {_exit_message}'
+        if error_detail:
+            _exit_text += f': {error_detail}'
+            logger.error(_exit_text)
+
         print('\n\n')
-        sys.stderr.write(_exit_text)
+        sys.stderr.write(_exit_text + '\n')
         return exit_code
 
     return exit_code
@@ -94,16 +95,13 @@ def parse_args(argv):
         description='Hardware Lister Windows Emulator',
     )
 
-    parser.add_argument(
-        '--json', '-j',
-        action='store_true',
-        help='output in JSON format (indented)'
-    )
+    parser.add_argument('--json', '-j', action='store_true', help='output in JSON format (indented)')
 
     parser.add_argument(
-        '--class-hw', '-c',
+        '--class-hw',
+        '-c',
         action='store',
-        help='print a specific hardware class (write "-c list" to get available classes)'
+        help='print a specific hardware class (write "-c list" to get available classes)',
     )
 
     return parser.parse_args()
@@ -132,8 +130,8 @@ def main(argv=None):
         _piece = args.class_hw.strip()
         _class = ''
         if _piece == 'list':
-            _help_msg = "Pieces of hardware to choice:\n"
-            for x, y in AVAILABLE_CLASSES.items():
+            _help_msg = 'Pieces of hardware to choice:\n'
+            for x, _ in AVAILABLE_CLASSES.items():
                 _help_msg += f'\t{x}\n'
 
             print(_help_msg)
@@ -145,8 +143,12 @@ def main(argv=None):
                 try:
                     hw_class = HardwareClass.factory(_class)()
                     formatted_data = hw_class.format_data(children=False)
-                except Exception:
-                    return _exit_manager(y[1], _class)
+                except wmi.x_access_denied as e:
+                    return _exit_manager(y[1], _class, f'Access denied: {e}')
+                except wmi.x_wmi as e:
+                    return _exit_manager(y[1], _class, f'WMI error: {e}')
+                except (AttributeError, KeyError, TypeError) as e:
+                    return _exit_manager(y[1], _class, str(e))
 
         if not _class:
             # parser.print_help()
@@ -157,8 +159,12 @@ def main(argv=None):
         try:
             hw_class = HardwareClass.factory('ComputerSystem')()
             formatted_data = hw_class.format_data(children=True)
-        except Exception:
-            return _exit_manager(16, 'system')
+        except wmi.x_access_denied as e:
+            return _exit_manager(16, 'system', f'Access denied: {e}')
+        except wmi.x_wmi as e:
+            return _exit_manager(16, 'system', f'WMI error: {e}')
+        except (AttributeError, KeyError, TypeError) as e:
+            return _exit_manager(16, 'system', str(e))
 
     if args.json:
         print(json.dumps(formatted_data, indent=2))
