ca4b7a6334665cfa7b65af30ed8702bb97df0f19
jact <jachavar@gmail.com>
2026-03-27 11:09:55 +0100

    fix(usb): internationalize device identification and filtering (P2.10)
    Replace localized Spanish strings for USB device identification and
    exclusion with language-neutral WMI properties (ClassGuid and Service).
    
    - Update properties_to_get to include ClassGuid and Service.
    - Use Service-based filtering (usbccgp, USBSTOR, usbhub) for exclusions.
    - Use ClassGuid-based identification for Keyboards, Mice, Disks, and Volumes.
    - Keep Caption-based fallback for specific products like SmartBoard.
    - Update unit tests to mock language-neutral properties.

--- lshw/classes/usb_device.py
+++ lshw/classes/usb_device.py
@@ -58,6 +58,8 @@ class UsbDevice(HardwareClass):
             'Description',
             'DeviceID',
             'PNPDeviceID',
+            'ClassGuid',
+            'Service',
         ]
 
         self._update_properties_to_return()
@@ -70,32 +72,22 @@ class UsbDevice(HardwareClass):
         Get Hardware set
         If self.dev_id exists get hardware for DeviceID
         """
-        # Devices excluded. We don't need them
-        device_excluded = [
-            'Dispositivo compuesto USB',
-            'Dispositivo de interfaz humana USB',
-            'Dispositivo de almacenamiento masivo USB',
-            'USB 2.0 Root Hub',
-        ]
+        # Exclude devices by Service or ClassGuid (Language Independent)
+        # usbccgp: Composite Device
+        # USBSTOR: Mass Storage
+        # usbhub: Root Hubs
+        excluded_services = {'usbccgp', 'USBSTOR', 'usbhub'}
+        # {745a17a0...}: Generic HID Class
+        excluded_guids = {'{745a17a0-74d3-11d0-b6fe-00a0c90f57da}'}
 
-        usb_controller_device = {}
         usb_controller_device_set = []
-
         usb_controller_device_list = []
-        usb_controller_device_primary = []
 
         for usb_assoc in self.wmi_system.Win32_USBControllerdevice(['Antecedent', 'Dependent']):
-            usb_controller_device['ant_value'] = (
-                usb_assoc.antecedent.split('=')[1].replace('"', '').replace('\\\\', '\\')
-            )
-            usb_controller_device['dep_value'] = (
-                usb_assoc.dependent.split('=')[1].replace('"', '').replace('\\\\', '\\')
-            )
-
-            # only get USB controllers (without duplicates)
-            usb_controller_device_list.append(usb_controller_device['ant_value'])
-
-            usb_controller_device_set.append(usb_controller_device.copy())
+            ant_value = usb_assoc.antecedent.split('=')[1].replace('"', '').replace('\\\\', '\\')
+            dep_value = usb_assoc.dependent.split('=')[1].replace('"', '').replace('\\\\', '\\')
+            usb_controller_device_list.append(ant_value)
+            usb_controller_device_set.append({'ant_value': ant_value, 'dep_value': dep_value})
 
         # remove duplicates
         usb_controller_device_primary = set(usb_controller_device_list)
@@ -111,32 +103,42 @@ class UsbDevice(HardwareClass):
                         'Win32_PNPEntity', f'PNPDeviceID="{self._sanitize_wql_value(element["dep_value"])}"'
                     )
                     for hw_item in self.wmi_system.query(wql):
-                        if hw_item.Caption not in device_excluded:
-                            for prop in self.properties_to_return:
-                                if prop != 'Parent_PNPDeviceID':
-                                    try:
-                                        self.properties_to_return[prop] = getattr(hw_item, prop)
-                                    except AttributeError:
-                                        self.properties_to_return[prop] = self.__DESC__
-                                else:
-                                    self.properties_to_return['Parent_PNPDeviceID'] = element['ant_value']
+                        service = getattr(hw_item, 'Service', '')
+                        guid = getattr(hw_item, 'ClassGuid', '')
 
-                            self.hardware_set_to_return.append(self.properties_to_return.copy())
+                        if service in excluded_services or guid in excluded_guids:
+                            continue
+
+                        props = self.properties_to_return.copy()
+                        for prop in props:
+                            if prop != 'Parent_PNPDeviceID':
+                                props[prop] = getattr(hw_item, prop, self.__DESC__)
+                            else:
+                                props['Parent_PNPDeviceID'] = element['ant_value']
+
+                        self.hardware_set_to_return.append(props)
 
     def _populate_hardware(self, item_ret: Hardware, hw_item: dict) -> Hardware:
         usb_id_device = 'usb_device'
+        guid = hw_item.get('ClassGuid', '')
         desc = hw_item.get('Description', self.__ERROR__)
+        caption = hw_item.get('Caption', '')
 
-        if desc == 'Mouse compatible con HID':
-            usb_id_device = 'usb_mouse'
-        elif desc == 'Dispositivo de teclado HID':
+        # Use ClassGuid for language-neutral identification
+        # Keyboards: {4d36e96b...}
+        # Mice: {4d36e96f...}
+        # Disks: {4d36e967...}
+        # Volumes: {71a27cdd...}
+        if guid == '{4d36e96b-e325-11ce-bfc1-08002be10318}':
             usb_id_device = 'usb_teclado'
-        elif desc == 'SmartBoard XX44':
-            usb_id_device = 'usb_smartboard_xx44'
-        elif desc == 'Unidad de disco':
+        elif guid == '{4d36e96f-e325-11ce-bfc1-08002be10318}':
+            usb_id_device = 'usb_mouse'
+        elif guid == '{4d36e967-e325-11ce-bfc1-08002be10318}':
             usb_id_device = 'usb_disk'
-        elif desc == 'Volumen genérico':
+        elif guid == '{71a27cdd-817f-11d0-bec7-08002be2092f}':
             usb_id_device = 'usb_vol'
+        elif 'SmartBoard' in caption:
+            usb_id_device = 'usb_smartboard_xx44'
 
         item_ret.id = usb_id_device
         item_ret.class_ = desc
@@ -145,5 +147,7 @@ class UsbDevice(HardwareClass):
         item_ret.pnpdeviceid = hw_item.get('PNPDeviceID', self.__ERROR__)
         item_ret.parent_pnpdeviceid = hw_item.get('Parent_PNPDeviceID', self.__ERROR__)
         item_ret.deviceid = hw_item.get('DeviceID', self.__ERROR__)
+        item_ret.class_guid = guid
+        item_ret.service = hw_item.get('Service', '')
 
         return item_ret
--- tests/test_usb_device.py
+++ tests/test_usb_device.py
@@ -14,6 +14,8 @@ def test_usb_device_basic(mock_wmi_connection):
     mock_pnp.Description = 'Mouse compatible con HID'
     mock_pnp.DeviceID = 'DEV_USB'
     mock_pnp.PNPDeviceID = 'USB\\VID_1234'
+    mock_pnp.ClassGuid = '{4d36e96f-e325-11ce-bfc1-08002be10318}'
+    mock_pnp.Service = 'mouhid'
 
     def mock_query(wql):
         if 'USB\\VID_1234' in wql:
@@ -39,7 +41,8 @@ def test_usb_device_excluded(mock_wmi_connection):
     mock_wmi_connection.Win32_USBControllerdevice.return_value = [mock_assoc]
 
     mock_pnp = MagicMock()
-    mock_pnp.Caption = 'USB 2.0 Root Hub'  # This should be excluded
+    mock_pnp.Caption = 'USB 2.0 Root Hub'  # Should be excluded
+    mock_pnp.Service = 'usbhub'
 
     def mock_query(wql):
         return [mock_pnp]
